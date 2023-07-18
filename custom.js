8f9cee2239c83f28d3890e7be02f1085fa3dac17
Furkan Oruç <20211129072@ogr.akdeniz.edu.tr>
2023-07-18 11:47:26 +0300

    update

--- custom.js
+++ custom.js
@@ -70,14 +70,15 @@ $(document).ready(function () {
     });
 
     var table = $('#example').DataTable({
-        "ajax": "/object.txt",
+        fixedHeader: true,
+        "ajax": "object.json",
         "rowId": 'id',
         "columns": [
             {
                 "className": 'dt-control',
                 "orderable": false,
                 "data": null,
-                "defaultContent": ''
+                "defaultContent": '',
             },
             { "data": "name" },
             { "data": "position" },
@@ -85,42 +86,23 @@ $(document).ready(function () {
             { "data": "start_date" },
         ],
         "order": [[1, 'asc']],
-        language: {
-            "emptyTable": "Tabloda herhangi bir veri mevcut değil",
-            "info": "_TOTAL_ kayıttan _START_ - _END_ arasındaki kayıtlar gösteriliyor",
-            "infoEmpty": "Kayıt yok",
-            "infoFiltered": "(_MAX_ kayıt içerisinden bulunan)",
-            "infoThousands": ".",
-            "lengthMenu": "_MENU_",
-            "loadingRecords": "Yükleniyor...",
-            "processing": "İşleniyor...",
-            "search": "",
-            "zeroRecords": "Eşleşen kayıt bulunamadı",
-            "paginate": {
-                "first": "İlk",
-                "last": "Son",
-                "next": "<i class='fas fa-chevron-right'>>",
-                "previous": "<i class='fas fa-chevron-left'><"
-            },
-            "aria": {
-                "sortAscending": ": artan sütun sıralamasını aktifleştir",
-                "sortDescending": ": azalan sütun sıralamasını aktifleştir"
-            },
-            "select": {
-                "rows": {
-                    "_": "%d kayıt seçildi",
-                    "1": "1 kayıt seçildi"
-                },
-                "cells": {
-                    "1": "1 hücre seçildi",
-                    "_": "%d hücre seçildi"
-                },
-                "columns": {
-                    "1": "1 sütun seçildi",
-                    "_": "%d sütun seçildi"
-                }
-            }
+        "language": {
+            "url": "languege.json"
         },
+        "pageLength": 25,
+        "drawCallback": function () {
+            const columns = [
+
+                { index: 1, style: { "background-color": "#212529" } },
+
+            ];
+            columns.forEach(col => {
+                $(`#example tbody tr td:nth-child(${col.index})`)
+                    .attr("data-title", col.title)
+                    .css(col.style || {});
+            });
+        },
+            
       
     });
 
